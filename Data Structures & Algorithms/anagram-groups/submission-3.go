08551ad5ed2daaf8bf7fe2476b2db0a548b1a2f1
tyoc213 <506234+tyoc213@users.noreply.github.com>
2026-08-09 13:49:44 -0600

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.go
@@ -0,0 +1,22 @@
+func groupAnagrams(strs []string) [][]string {
+	result := [][]string{};
+	hashmap := make(map[uint64][]string);
+	for _, s := range strs {
+		counts := [26]int{}
+		for _, c := range(s) {
+			counts[c-'a'] += 1
+		}
+
+		var hash uint64
+		for _, x := range counts {
+			hash ^= uint64(x)
+			hash *= 16777619 // FNV prime
+		}
+		hashmap[hash] = append(hashmap[hash], s)
+	}
+	for _, v := range hashmap {
+		result = append(result, v)
+	}
+
+	return result;
+}

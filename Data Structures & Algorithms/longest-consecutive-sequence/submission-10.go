db581b56cdbcd616d4e0dfd0ffa30197cacbf06a
tyoc213 <506234+tyoc213@users.noreply.github.com>
2026-08-14 14:29:20 -0600

    Add: longest-consecutive-sequence - submission-10

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-10.go
@@ -0,0 +1,16 @@
+func longestConsecutive(nums []int) int {
+    if len(nums) <= 1 { return len(nums)}
+    sort.Ints(nums)
+    count := 1
+    maxint := 0
+    for idx := 1; idx<len(nums); idx++ {
+        diff := nums[idx]-nums[idx-1]
+        if diff > 1 {
+            count = 1
+        } else {
+            count += diff
+        }        
+        maxint = max(maxint, count)
+    }
+    return maxint
+}
